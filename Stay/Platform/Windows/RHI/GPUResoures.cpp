441984930bc4705cf185337236e5317f163921de
wuching <mr.wuching@gmail.com>
2024-01-09 20:17:01 +0800

    ADD: GPUsource Upload Buffer

---
+++ Stay/Platform/Windows/RHI/GPUResoures.cpp
@@ -0,0 +1,187 @@
+
+#include "../WinError.h"
+
+
+#include "GPUResoures.h"
+#include "GraphicsCore.h"
+
+namespace stay
+{
+	GPUResoures::GPUResoures(ID3D12Resource* resource, D3D12_RESOURCE_STATES transitioningStatus, D3D12_RESOURCE_STATES usageStatus)
+	{
+		if (resource != nullptr)
+		{
+			m_gpuAddress = resource->GetGPUVirtualAddress();
+		}
+
+		m_transitioningStatus = transitioningStatus;
+		m_usageStatus = usageStatus;
+	}
+
+	void GPUResoures::Clear()
+	{
+		m_resource = nullptr;
+		m_transitioningStatus = D3D12_RESOURCE_STATE_COMMON;
+		m_usageStatus = D3D12_RESOURCE_STATE_COMMON;
+		m_gpuAddress = (D3D12_GPU_VIRTUAL_ADDRESS)CPU_GPU_VIRTUAL_ADDRESS_NULL;
+	}
+
+	void UploadBuffer::Create(const std::wstring& filename, size_t size, void* pData )
+	{
+		m_size = size;
+		m_usageStatus = D3D12_RESOURCE_STATE_COMMON;
+		m_transitioningStatus = m_usageStatus;
+
+		D3D12_HEAP_PROPERTIES heapPro{};
+		heapPro.Type = D3D12_HEAP_TYPE_UPLOAD;
+		heapPro.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapPro.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+
+
+		D3D12_RESOURCE_DESC rsDesc{};
+		rsDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		rsDesc.Alignment = 0;
+		rsDesc.Width = m_size;
+		rsDesc.Height = 1;
+		rsDesc.DepthOrArraySize = 1;
+		rsDesc.MipLevels = 1;
+		rsDesc.Format = DXGI_FORMAT_UNKNOWN;
+		rsDesc.SampleDesc.Count = 1;
+		rsDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+
+		THROW_IF_FAILED(
+			Graphics::g_Device->CreateCommittedResource(&heapPro, D3D12_HEAP_FLAG_NONE, &rsDesc,
+				m_usageStatus,
+				nullptr, IID_PPV_ARGS(&m_resource)));
+
+		m_gpuAddress = m_resource->GetGPUVirtualAddress();
+
+		if (pData == nullptr)
+		{
+			return;
+		}
+		
+		auto pBuffer = Map();
+
+		memcpy(pBuffer, pData, m_size);
+
+#ifdef _RELEASE
+		(filename);
+#elif _DEBUG
+		m_resource->SetName(filename.c_str());
+#endif // RELEASE
+
+	}
+
+	void* UploadBuffer::Map()
+	{
+		if (m_mapAddress != nullptr)
+		{
+			return m_mapAddress;
+		}
+
+		D3D12_RANGE mapRange = { 0, m_size };
+
+		THROW_IF_FAILED(m_resource->Map(0, &mapRange, &m_mapAddress));
+		return m_mapAddress;
+	}
+
+	void UploadBuffer::Unmap(size_t begin, size_t end)
+	{
+		D3D12_RANGE unmapRange = { begin, std::min(end, m_size) };
+
+		m_resource->Unmap(0, &unmapRange);
+	}
+
+	void GPUBuffer::Create(const std::wstring& name)
+	{
+		(name);
+	}
+
+	void GPUBuffer::Create(const std::wstring& name, size_t numElements, size_t elementSize, ID3D12Resource* pData)
+	{
+		m_elementCount = numElements;
+		m_elementSize = elementSize;
+		m_bufferSize = m_elementCount * m_elementSize;
+		
+		m_usageStatus = D3D12_RESOURCE_STATE_COMMON;
+		m_transitioningStatus = m_usageStatus;
+
+		D3D12_HEAP_PROPERTIES heapPro{};
+		heapPro.Type = D3D12_HEAP_TYPE_UPLOAD;
+		heapPro.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+		heapPro.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+
+
+		D3D12_RESOURCE_DESC rsDesc{};
+		rsDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		rsDesc.Alignment = 0;
+		rsDesc.Width = m_bufferSize;
+		rsDesc.Height = 1;
+		rsDesc.DepthOrArraySize = 1;
+		rsDesc.MipLevels = 1;
+		rsDesc.Format = DXGI_FORMAT_UNKNOWN;
+		rsDesc.SampleDesc.Count = 1;
+		rsDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+
+		THROW_IF_FAILED(
+			Graphics::g_Device->CreateCommittedResource(&heapPro, D3D12_HEAP_FLAG_NONE, &rsDesc,
+				m_usageStatus,
+				nullptr, IID_PPV_ARGS(&m_resource)));
+
+		m_gpuAddress = m_resource->GetGPUVirtualAddress();
+		
+		if (pData != nullptr)
+		{
+			CommandList::InitializeBuffer(L"initialize Buffer", m_resource.Get(), pData);
+		}
+
+#ifdef _RELEASE
+		(name);
+#elif _DEBUG
+		m_resource->SetName(name.c_str());
+#endif // RELEASE
+
+
+	}
+
+	void GPUBuffer::Create(const std::wstring& name, size_t numElements, size_t elementSize, UploadBuffer& uploadBuffer)
+	{
+		Create(name, numElements, elementSize, uploadBuffer.GetResource());
+	}
+
+	D3D12_VERTEX_BUFFER_VIEW GPUBuffer::VertexBufferView(UINT offset, UINT SizeInBytes, UINT StrideInBytes) const
+	{
+		D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+		vertexBufferView.BufferLocation = m_gpuAddress + offset;
+		vertexBufferView.SizeInBytes = SizeInBytes;
+		vertexBufferView.StrideInBytes = StrideInBytes;
+
+		return vertexBufferView;
+	}
+
+
+	D3D12_INDEX_BUFFER_VIEW GPUBuffer::IndexBufferView(UINT offset, UINT SizeInBytes, DXGI_FORMAT Format) const
+	{
+		D3D12_INDEX_BUFFER_VIEW indexBufferView{};
+		indexBufferView.BufferLocation = m_gpuAddress + offset;
+		indexBufferView.SizeInBytes = SizeInBytes;
+		indexBufferView.Format = Format;
+
+		return indexBufferView;
+	}
+
+	D3D12_CONSTANT_BUFFER_VIEW_DESC GPUBuffer::ConstantBufferView(UINT offset, UINT SizeInBytes)
+	{
+		D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc{};
+		cbvDesc.BufferLocation = m_gpuAddress + offset;
+		cbvDesc.SizeInBytes = SizeInBytes;
+
+		//Graphics::g_Device->CreateConstantBufferView(&cbvDesc, nullptr);
+		return D3D12_CONSTANT_BUFFER_VIEW_DESC();
+	}
+
+
+}
---
+++ Stay/Platform/Windows/RHI/GPUResoures.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "../WinStd.h"
+#include "../ClassAttribute.h"
+
+namespace stay
+{
+	class GPUResoures : public NoCopyable
+	{
+	public:
+		GPUResoures() { Clear(); };
+		~GPUResoures() { Destory(); };
+
+		GPUResoures(ID3D12Resource* resource, D3D12_RESOURCE_STATES transitioningStatus, D3D12_RESOURCE_STATES usageStatus = D3D12_RESOURCE_STATE_COMMON);
+		virtual D3D12_RESOURCE_BARRIER_TYPE GetResourceBarrierType() { return D3D12_RESOURCE_BARRIER_TYPE_TRANSITION; }
+		virtual void Destory() { Clear(); };
+
+		ID3D12Resource* GetResource() const { return m_resource.Get(); }
+		D3D12_RESOURCE_STATES GetCurrentStatus() const { return m_usageStatus; }
+		void SetCurrentStatus(D3D12_RESOURCE_STATES states) { m_usageStatus = states; }
+		void SetTransitioningStatus(D3D12_RESOURCE_STATES states) { m_transitioningStatus = states; }
+		D3D12_RESOURCE_STATES GetTransitioningStatus() const { return m_transitioningStatus; }
+		D3D12_GPU_VIRTUAL_ADDRESS GetGPUVirtualAddress() const { return  m_gpuAddress; }
+	protected:
+		template <typename T>
+		using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+		void Clear();
+
+
+		D3D12_GPU_VIRTUAL_ADDRESS m_gpuAddress;
+		D3D12_RESOURCE_STATES m_transitioningStatus;
+		D3D12_RESOURCE_STATES m_usageStatus;
+		ComPtr<ID3D12Resource> m_resource;
+	};
+
+	class UploadBuffer : public GPUResoures
+	{
+	public:
+		UploadBuffer() = default;
+		~UploadBuffer() { Destory(); };
+
+		void Create(const std::wstring& filename, size_t size, void* pData = nullptr);
+		void* Map();
+		void Unmap(size_t begin = 0u, size_t end = -1);
+	private:
+		void* m_mapAddress = nullptr;
+		size_t m_size;
+	};
+
+
+	class GPUBuffer : public GPUResoures
+	{
+	public:
+		GPUBuffer() = default;
+		~GPUBuffer() = default;
+
+		void Create(const std::wstring& name);
+		void Create(const std::wstring& name, size_t numElements, size_t elementSize, ID3D12Resource* pData = nullptr);
+		void Create(const std::wstring& name, size_t numElements, size_t elementSize, UploadBuffer& uploadBuffer);
+
+		D3D12_VERTEX_BUFFER_VIEW VertexBufferView(UINT offset, UINT SizeInBytes, UINT StrideInBytes) const;
+		D3D12_VERTEX_BUFFER_VIEW VertexBufferView(UINT SizeInBytes, UINT StrideInBytes) const { return VertexBufferView(0u, SizeInBytes, StrideInBytes); }
+		D3D12_INDEX_BUFFER_VIEW IndexBufferView(UINT offset, UINT SizeInBytes, DXGI_FORMAT Format = DXGI_FORMAT_R32_UINT) const;
+		D3D12_INDEX_BUFFER_VIEW IndexBufferView(UINT SizeInBytes, DXGI_FORMAT Format = DXGI_FORMAT_R32_UINT) const { return IndexBufferView(0, SizeInBytes, Format); };
+
+		D3D12_CONSTANT_BUFFER_VIEW_DESC ConstantBufferView(UINT offset, UINT SizeInBytes);
+
+	protected:
+		size_t m_elementCount;
+		size_t m_elementSize;
+		size_t m_bufferSize;
+	};
+}
